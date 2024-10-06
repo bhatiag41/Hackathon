03c83c5a6651e12c39516a0c2adcad591fcd9e15
YashGoswamiGOAT <yashgoswamigoat@gmail.com>
2024-10-06 11:50:15 +0530

    Initial Commet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Landing from "./component/Landing";
+import GraphDisplay from "./component/GraphDisplay";
+import {Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+  const [graphType,setGraphType] = useState('') ;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App" >
+        <Routes >
+          <Route path="/" element={<Landing setGraph={setGraphType}/>} />
+          <Route path="/graphDisplay" element={<GraphDisplay graphType={graphType} />} />
+        </Routes>
+      </div>
   );
 }
 
